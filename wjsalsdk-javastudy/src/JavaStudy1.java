0a68b9bef4ad1a3e4ff27b2d99894676f1e654c0
wjsal <wjsal@MinA>
2021-07-25 14:23:58 +0900

    zx

--- wjsalsdk-javastudy/src/JavaStudy1.java
+++ wjsalsdk-javastudy/src/JavaStudy1.java
@@ -1,9 +1,19 @@
 public class JavaStudy1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String me = "CSY";
-		System.out.println(me);
+		// TODO Auto-generated method
+		//정수형으로 변수 선언(int)
+		//문자열로 변수 선언(string)
+		//데이터 출력 실행문
+		int a = 89;
+		String b = "c";
+		System.out.println(a + b);
+		System.out.println("");
+		System.out.println(b);
+		
+		String n = "89";
+		System.out.println(a+n);
+		
 	}
 
 }
