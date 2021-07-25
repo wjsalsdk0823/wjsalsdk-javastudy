134021c152f444aae84ea79bbd0b85887573f492
wjsal <wjsal@MinA>
2021-07-25 21:58:15 +0900

    111

--- wjsalsdk-javastudy/src/JavaStudy2.java
+++ wjsalsdk-javastudy/src/JavaStudy2.java
@@ -1,8 +1,14 @@
 public class JavaStudy2 {
 
 	public static void main(String[] args) {
+	// 변수 선언을 통해 2*4=8 를 출력.(결과 값 8만 나오게)
+	int a = 2;
+	int x = 4;
+	String v = "2 * 4 =";
+	System.out.println(v + a * x );//첫번째 방법
 	
-		
+	System.out.println("2 * 4 ="+ a*x);//두번째 방법
+    
 	}
 
 }
--- wjsalsdk-javastudy/src/JavaStudy3.java
+++ wjsalsdk-javastudy/src/JavaStudy3.java
@@ -1,15 +1,24 @@
-import java.util.Scanner;
-
 public class JavaStudy3 {
 
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		String str=s.nextLine();		
-		if(str.equalsIgnoreCase("CSY")) {
-			System.out.println("CHOI S Y");
-		}
+	//if ( 참 , 거짓을 판별 해 구현내용을 실행)
+		//값이 60이하일 때 최서영 바보를 출력.
+		//else if 로 조건 추가
 		
-		s.close();
-	}
+		int a = 70;
+		
+		if(a <= 60) {
+			System.out.println("최서영 바보");
+		}
+		else if(a<=50) {
+			System.out.println("최서영 바보2");
+		}
+			else {
+				System.out.println("ㅇㅇ");
+			}
+		}
+		}
+			
 
-}
+
+
